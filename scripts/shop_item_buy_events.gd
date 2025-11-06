a01643ed12d22591a692058ea24bee9483069c68
tommyrobot666 <tommyrobot10@gmail.com>
2025-11-06 00:40:12 -0800

    added function to help with adding color bits

--- scripts/shop_item_buy_events.gd
+++ scripts/shop_item_buy_events.gd
@@ -9,8 +9,16 @@ func test():
 func buy_red():
 	CollectedResources.change_color(CollectedResources.Types.RED,-5)
 	print("item bought")
-	var new_color_bit = ColorBit.new()
+	var new_color_bit = create_and_add_color_bit()
 	new_color_bit.bounce_spread = 11.67
 	new_color_bit.gravity = 200
+
+
+func create_and_add_color_bit(
+	color:CollectedResources.Types=CollectedResources.Types.RED,
+	size:float = 10) -> ColorBit:
+	var new_color_bit = ColorBit.new()
+	new_color_bit.color = color
+	new_color_bit.size = size
 	Engine.get_main_loop().current_scene.get_node("/root/Game/Node2D/Colors").add_child(new_color_bit)
-	#%Colors.add_child(new_color_bit)
+	return new_color_bit
